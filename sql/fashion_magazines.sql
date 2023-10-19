57e341f3174ac1bd643250b5a2f750a5bce38e98
Alex Tortorella <alexrella48@gmail.com>
2023-10-19 13:42:06 -0400

    Fashion Magazine Project Submission

--- sql/fashion_magazines.sql
+++ sql/fashion_magazines.sql
@@ -1,1 +1,9 @@
--- Add your sql here
+SELECT customer_name AS 'Customer', 
+PRINTF('$%.2f', SUM(subscriptions.subscription_length * subscriptions.price_per_month)) AS 'Amount Due'
+FROM orders 
+JOIN customers 
+ON orders.customer_id = customers.customer_id
+JOIN subscriptions 
+ON orders.subscription_id = subscriptions.subscription_id
+WHERE subscriptions.description = 'Fashion Magazine' AND order_status = 'unpaid'
+GROUP BY customers.customer_name;
